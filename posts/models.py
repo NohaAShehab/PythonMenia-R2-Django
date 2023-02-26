5b6e0c8ce7f040c91c43a86aa69fc35d94bf3fc9
Noha Shehab <nohashehab.iti@gmail.com>
2023-02-26 12:01:15 +0200

    creating model , migrations

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+## Model class represent table in the database
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description  = models.TextField()
+    image = models.CharField(max_length=100)
+    version = models.IntegerField()
+    publisher_email= models.EmailField()
+    privacy = models.CharField(max_length=2, choices=[(1, 'Public'),(2, 'Private')])
+
